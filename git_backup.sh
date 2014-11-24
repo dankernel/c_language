504ac581e15871a76e41fc945b472ffde7896ea6
dankernel <google@dankook.ac.kr>
2014-11-25 04:51:40 +0900

    auto-20141125-045140

--- git_backup.sh
+++ git_backup.sh
@@ -4,7 +4,7 @@ WORK=1
 find -size +40M -not -path "./" | grep -q "\." && WORK=0
 
 if [ $WORK = 1 ]; then
-  echo -e '\E[34m'"\033[1m[O  K]40M\033[0m"
+  echo -e '\E[34m'"\033[1m[O  K] 40M\033[0m"
 
   if [ ! -d .git/ ]
   then
